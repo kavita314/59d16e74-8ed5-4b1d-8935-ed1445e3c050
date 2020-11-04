602838b0064b1416ae0577b68a44ba3504aea734
kavita314 <45900067+kavita314@users.noreply.github.com>
2020-11-04 20:09:29 +0530

    Update Demo.java
    Done

--- src/main/java/javaApp/Demo.java
+++ src/main/java/javaApp/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
     public static void main(String[] args) {
       
         //Insert your code here
+        System.out.println("Welcome to  first maven war project");
     }
 
 }
